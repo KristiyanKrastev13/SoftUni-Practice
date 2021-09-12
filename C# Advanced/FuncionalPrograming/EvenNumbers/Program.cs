6c9b5b9dac4b5421da9b857e007b2ff4d5464d08
KristiyanKrastev13 <kristiqn4o1@gmail.com>
2021-09-12 17:39:57 +0200

    update

---
+++ C# Advanced/FuncionalPrograming/EvenNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+
+            Console.WriteLine(String.Join(", ", array));
+        }
+    }
+}
