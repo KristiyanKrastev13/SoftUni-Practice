b95902fd1892398c5ee3ba74e57b80dd9816a55c
KristiyanKrastev13 <kristiqn4o1@gmail.com>
2021-10-21 23:10:51 +0200

    update

---
+++ C# Advanced/Exercise/StreetRacer/Race.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxhp)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxhp;
+            Participants = new List<Car>();
+        }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+        public List<Car> Participants { get; set; }
+        public int Count => Participants.Count;
+
+        public void Add(Car car)
+        {
+            string licence = car.LicensePlate;
+            Car contained = Participants.FirstOrDefault(c => c.LicensePlate == licence);
+            if (contained == null && Participants.Count + 1 <= Capacity)
+            {
+                Participants.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            Car isValid = Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+            if (isValid != null)
+            {
+                Participants.Remove(isValid);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            Car isValid = Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+            if (isValid != null)
+            {
+                Participants.Remove(isValid);
+                return isValid;
+            }
+            else
+            {
+                return isValid;
+            }
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            Car isValid = Participants.OrderByDescending(c => c.HorsePower).FirstOrDefault();
+            return isValid;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var car in Participants)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ C# Advanced/Exercise/battleOfTheFiveArmies/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace battleOfTheFiveArmies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[rows][];
+            int armyRow = -1;
+            int armyCol = -1;
+            bool win = false;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string currCol = Console.ReadLine();
+                matrix[row] = new char[currCol.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {    
+                    matrix[row][col] = currCol[col];
+                    if (matrix[row][col] == 'A')
+                    {
+                        armyRow = row;
+                        armyCol = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+                string direction = command[0];
+                int enemyRow = int.Parse(command[1]);
+                int enemyCol = int.Parse(command[2]);
+                matrix[enemyRow][enemyCol] = 'O';
+                energy--;
+                if (direction == "up")
+                {
+                    matrix[armyRow][armyCol] = '-';
+                    if (ValidLocation(matrix, armyRow - 1, armyCol))
+                    {                       
+                        armyRow -= 1;
+                        if (matrix[armyRow][armyCol] == 'O')
+                        {
+                            energy -= 2;
+                        }
+                        else if (matrix[armyRow][armyCol] == 'M')
+                        {
+                            matrix[armyRow][armyCol] = '-';
+                            win = true;
+                            break;
+                        }
+                    }
+                    if (energy <= 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'X';
+                        break;
+                    }
+                    else if (energy > 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'A';
+                    }
+                }
+                if (direction == "down")
+                {
+                    matrix[armyRow][armyCol] = '-';
+                    if (ValidLocation(matrix, armyRow + 1, armyCol))
+                    {
+                        armyRow += 1;
+                        if (matrix[armyRow][armyCol] == 'O')
+                        {
+                            energy -= 2;
+                        }
+                        else if (matrix[armyRow][armyCol] == 'M')
+                        {
+                            matrix[armyRow][armyCol] = '-';
+                            win = true;
+                            break;
+                        }
+                    }
+                    if (energy <= 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'X';
+                        break;
+                    }
+                    else if (energy > 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'A';
+                    }
+                }
+                if (direction == "left")
+                {
+                    matrix[armyRow][armyCol] = '-';
+                    if (ValidLocation(matrix, armyRow, armyCol - 1))
+                    {
+                        armyCol -= 1;
+                        if (matrix[armyRow][armyCol] == 'O')
+                        {
+                            energy -= 2;
+                        }
+                        else if (matrix[armyRow][armyCol] == 'M')
+                        {
+                            matrix[armyRow][armyCol] = '-';
+                            win = true;
+                            break;
+                        }
+                    }
+                    if (energy <= 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'X';
+                        break;
+                    }
+                    else if (energy > 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'A';
+                    }
+                }
+                if (direction == "right")
+                {
+                    matrix[armyRow][armyCol] = '-';
+                    if (ValidLocation(matrix, armyRow, armyCol + 1))
+                    {
+                        armyCol += 1;
+                        if (matrix[armyRow][armyCol] == 'O')
+                        {
+                            energy -= 2;
+                        }
+                        else if (matrix[armyRow][armyCol] == 'M')
+                        {
+                            matrix[armyRow][armyCol] = '-';
+                            win = true;
+                            break;
+                        }
+                    }
+                    if (energy <= 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'X';
+                        break;
+                    }
+                    else if (energy > 0 && win == false)
+                    {
+                        matrix[armyRow][armyCol] = 'A';
+                    }
+                    if (win)
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            if (win)
+            {
+                Console.WriteLine($"The army managed to free the Middle World! Armor left: {energy}");
+            }
+            else
+            {
+                Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+            }
+            Print(matrix);
+
+        }
+        public static void Print(char[][] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+              
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool ValidLocation(char[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+    }
+}
